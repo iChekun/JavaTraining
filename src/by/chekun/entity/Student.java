427d7445b83a9404fe1be1eeef5823cd5972c8e3
nykec <ilya_chekun@mail.ru>
2019-05-19 20:08:07 +0300

    task_02

---
+++ src/by/chekun/entity/Student.java
@@ -0,0 +1,34 @@
+package by.chekun.entity;
+
+import java.util.Objects;
+
+public class Student {
+
+    public String name;
+    public int mark;
+
+    public Student(String name,int mark){
+        this.name=name;
+        this.mark=mark;
+    }
+
+    @Override
+    public String toString(){
+        return name + " : " + mark;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return mark == student.mark &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, mark);
+    }
+}
---
+++ src/by/chekun/starter/Task02.java
@@ -0,0 +1,21 @@
+package by.chekun.starter;
+
+
+import by.chekun.typesTester.DataTypesTester;
+
+public class Task02 {
+
+    public static void main(String[] args) {
+
+        DataTypesTester.testInt();
+        DataTypesTester.testBoolean();
+        DataTypesTester.testLong();
+        DataTypesTester.testDouble();
+        DataTypesTester.testShort();
+        DataTypesTester.testByte();
+        DataTypesTester.testChar();
+        DataTypesTester.testFloat();
+        DataTypesTester.testString();
+        DataTypesTester.testObj();
+    }
+}
