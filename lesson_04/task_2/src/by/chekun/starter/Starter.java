78321435ed6adc1c541e319c047eb1826fb543cb
Ilya Chekun <ilya_chekun@mail.ru>
2019-05-27 00:55:14 +0300

    ..

---
+++ lesson_04/task_2/src/by/chekun/starter/Starter.java
@@ -0,0 +1,27 @@
+package by.chekun.starter;
+
+import by.chekun.action.circleAction.CircleLogic;
+import by.chekun.action.ringAction.RingLogic;
+import by.chekun.printer.Printer;
+
+/**
+ * На плоскости даны два круга с общим центром и радиусами R1 и R2 (R1 > R2).
+ * Разработайте программу нахождения площади кольца,
+ * внешний радиус которого равен R1,  а внутренний радиус равен R2.
+ */
+
+public class Starter {
+
+    public static void main(String[] argc) {
+        double R1 = 10;
+        double R2 = 5;
+
+        double firstCircleSquare = CircleLogic.calculateCircleSquare(R1);
+        double secondCircleSquare = CircleLogic.calculateCircleSquare(R2);
+        double ringSquare = RingLogic.calculateRingSquare(firstCircleSquare, secondCircleSquare);
+
+        Printer.println("Площадь внешнего круга: "+firstCircleSquare+
+                "\nПлощадь внутреннего круга: "+secondCircleSquare+
+                "\nПлощадь кольца: " + ringSquare);
+    }
+}
